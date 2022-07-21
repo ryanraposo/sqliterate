6718b510eb4bc6c238ff4c3bb23380c71925a590
Ryan Raposo <raposo.ryan@gmail.com>
2022-07-21 07:34:00 -0400

    create landing page

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ app.use(express.json());
 const PORT = process.env.PORT || 3001;
 
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
